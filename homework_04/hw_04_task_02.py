7096125edc9954a321792b47c5e602f874e4e56e
Tatsiana Malafei <tmolofei@yandex.by>
2022-11-10 00:11:18 +0300

    added changes in task_02 from homework_04

--- homework_04/hw_04_task_02.py
+++ homework_04/hw_04_task_02.py
@@ -3,11 +3,18 @@
 # «Введите число: »  24
 # Сумма чисел → 6
 
+"""version_1"""
 user_num = int(input('Enter any number: '))
 COUNT = 0
-
 while user_num > 0:
     COUNT += user_num % 10
     user_num //= 10
+print(f'Sum of digits of entered number is {COUNT}')
 
+"""version_2"""
+user_num = input('Enter any number: ')
+COUNT = 0
+
+for i in user_num:
+    COUNT += int(i)
 print(f'Sum of digits of entered number is {COUNT}')
